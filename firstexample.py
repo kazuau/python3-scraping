d5e2a0be6de4590670b983d3a8d79256a70695c7
UENO Kazuaki <kazuau@gmail.com>
2020-01-09 02:01:02 +0000

    upto section 2.3

--- firstexample.py
+++ firstexample.py
@@ -4,3 +4,9 @@ url = 'http://www.webscrapingfordatascience.com/basichttp/'
 #url = 'http://www.google.com/'
 r = requests.get(url)
 print(r.text)
+
+print(r.status_code)
+print(r.reason)
+print(r.headers)
+print(r.request)
+print(r.request.headers)
